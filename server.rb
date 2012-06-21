44fe9f3517f9adab6646529c8a7295237b5edfc4
Jun Yokoyama <yokoyama@larus.jp>
2012-06-22 01:26:39 +0900

    スキーマにcreated_at,updated_atを追加し、追加日時を表示するようにした

--- server.rb
+++ server.rb
@@ -6,10 +6,13 @@ Sequel::Model.plugin(:schema)
 
 Sequel.sqlite('db/diptych.db')
 class Entries < Sequel::Model
+  plugin :timestamps, :update_on_create => true
   unless table_exists?
     set_schema do
       primary_key :id
       string :url
+      timestamp :created_at
+      timestamp :updated_at
     end
     create_table
   end
@@ -22,7 +25,7 @@ get '/' do
 end
 
 post '/add' do
-  @url = params[:url]
-  Entries.insert :url => @url
+  @entry = Entries.new :url => params[:url]
+  @entry.save
   redirect '/'
 end
