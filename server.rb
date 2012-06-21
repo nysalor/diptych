2be1e0e5333a9d6a67ce16d20495aa58a2514f18
Jun Yokoyama <jun@larus.org>
2012-06-21 21:21:21 +0900

    Sequelのスキーマを定義した

--- server.rb
+++ server.rb
@@ -2,6 +2,19 @@ if development?
   require 'sinatra/reloader'
 end
 
+Sequel::Model.plugin(:schema)
+
+Sequel.sqlite('db/diptych.db')
+class Entries < Sequel::Model
+  unless table_exists?
+    set_schema do
+      primary_key :id
+      string :url
+    end
+    create_table
+  end
+end
+
 get '/' do
   @title = 'diptych - TOP'
   haml :index
